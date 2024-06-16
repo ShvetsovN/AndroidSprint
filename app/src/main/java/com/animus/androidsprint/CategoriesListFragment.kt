48b352624979a1f809147b397bcd1be9a28ab165
= <=>
2024-06-16 15:29:52 +0500

    RA-04 - solution added

---
+++ app/src/main/java/com/animus/androidsprint/CategoriesListFragment.kt
@@ -0,0 +1,30 @@
+package com.animus.androidsprint
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.animus.androidsprint.databinding.FragmentListCategoriesBinding
+
+class CategoriesListFragment : Fragment() {
+
+    private var _binding: FragmentListCategoriesBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding for FragmentListCategoriesBinding must not be null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentListCategoriesBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
