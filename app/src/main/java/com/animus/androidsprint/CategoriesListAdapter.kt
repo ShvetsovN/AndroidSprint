81e70656a29a1fe977ed13763303af168a0fcc70
= <=>
2024-06-21 18:59:34 +0500

    RA-08-RecycleView-categories Added cardview elements Added montserrat_medium font Started create CategoriesListAdapter

---
+++ app/src/main/java/com/animus/androidsprint/CategoriesListAdapter.kt
@@ -0,0 +1,50 @@
+package com.animus.androidsprint
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.animus.androidsprint.databinding.FragmentListCategoriesBinding
+import com.animus.androidsprint.databinding.ItemCategoryBinding
+
+class CategoriesListAdapter(private val dataSet: List<Category>) :
+    RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
+
+    class ViewHolder(private val binding: ItemCategoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(itemView: Category) {
+            with(binding){
+                cvId
+                ivItemImage
+                tvItemHeader
+                tvItemDescription
+            }
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.text_row_item, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.textView.text = dataSet[position]
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
+}
+
+
