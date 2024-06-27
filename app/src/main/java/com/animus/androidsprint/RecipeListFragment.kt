6324c8dda6870b862fb2a9a2e53e5780fa50e348
= <=>
2024-06-27 11:56:04 +0500

    RA-10 solution added

--- app/src/main/java/com/animus/androidsprint/RecipeListFragment.kt
+++ app/src/main/java/com/animus/androidsprint/RecipeListFragment.kt
@@ -11,18 +11,17 @@ class RecipeListFragment : Fragment() {
     private var categoryId: Int? = null
     private var categoryName: String? = null
     private var categoryImageUrl: String? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+
+    ): View? {
         arguments?.let {
             categoryId = it.getInt("CATEGORY_ID")
             categoryName = it.getString("CATEGORY_NAME")
             categoryImageUrl = it.getString("CATEGORY_IMAGE_URL")
         }
-    }
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
         return inflater.inflate(R.layout.fragment_recipe_list, container, false)
     }
 }
