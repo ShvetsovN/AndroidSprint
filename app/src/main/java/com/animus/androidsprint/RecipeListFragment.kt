10dfb6548a0bca0e21eea8bff0e984f3e02adae2
= <=>
2024-07-03 12:37:56 +0500

    RA-11 solution changed 1. changed fragment_list_categories.xml tv text 2. the name of the two strings elements has been changed 3. image call for categoryImageUrl(RecipeListFragment) changed

--- app/src/main/java/com/animus/androidsprint/RecipeListFragment.kt
+++ app/src/main/java/com/animus/androidsprint/RecipeListFragment.kt
@@ -1,7 +1,9 @@
 package com.animus.androidsprint
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.text.TextUtils.replace
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +14,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.animus.androidsprint.databinding.FragmentListCategoriesBinding
 import com.animus.androidsprint.databinding.FragmentRecipeBinding
 import com.animus.androidsprint.databinding.FragmentRecipeListBinding
+import java.io.IOException
+import java.io.InputStream
 
 class RecipeListFragment : Fragment() {
 
@@ -41,8 +45,14 @@ class RecipeListFragment : Fragment() {
         categoryName?.let {
             binding.tvHeaderRicipeList.text = it
         }
-        categoryImageUrl?.let {
-            binding.ivHeaderRecipeList.setImageResource(R.drawable.bcg_categories)
+        categoryImageUrl?.let{ imageUrl ->
+            try {
+                val inputStream: InputStream = binding.ivHeaderRecipeList.context.assets.open(imageUrl)
+                val drawable = Drawable.createFromStream(inputStream, null)
+                binding.ivHeaderRecipeList.setImageDrawable(drawable)
+            } catch (ex: IOException){
+                Log.e("RLF.onViewCreated", "Error loading image from assets")
+            }
         }
         initRecycle()
     }
