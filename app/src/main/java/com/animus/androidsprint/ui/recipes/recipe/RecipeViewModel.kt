8c5ca36d2a580958b15cb25523a1f34009227c25
= <=>
2024-07-22 14:56:12 +0500

    RA-20 solution added 1. Added RecipeState and Ingredient data class in RecipeViewModel.kt

--- app/src/main/java/com/animus/androidsprint/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/animus/androidsprint/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,7 +1,21 @@
 package com.animus.androidsprint.ui.recipes.recipe
 
 import androidx.lifecycle.ViewModel
+import com.animus.androidsprint.model.Ingredient
+
+data class RecipeState(
+    var id: Int? = null,
+    var title: String? = "",
+    var ingredients: List<Ingredient>? = listOf(),
+    var method: List<String>? = listOf(),
+    var imageUrl: String? = "",
+    )
+
+data class Ingredient(
+    val quantity: String? = "",
+    val unitOfMeasure: String? = "",
+    val description: String? = "",
+)
 
 class RecipeViewModel : ViewModel(){
-
 }
