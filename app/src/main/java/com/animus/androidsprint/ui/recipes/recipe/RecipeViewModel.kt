5292a39f6c0f9736459c31e598b67929fcd6069b
= <=>
2024-07-23 12:08:08 +0500

    RA-20 solution added 1. Changed RecipeState values

--- app/src/main/java/com/animus/androidsprint/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/animus/androidsprint/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,17 +1,12 @@
 package com.animus.androidsprint.ui.recipes.recipe
 
 import androidx.lifecycle.ViewModel
-import com.animus.androidsprint.model.Ingredient
 
 data class RecipeState(
     var numberOfPortion: Int? = null,
-    var ingredients: List<Ingredient>? = listOf(),
-    var imageUrl: String? = null,
+    var quantity: String? = "",
+    var isFavorite: Boolean? = null,
     )
-
-data class Ingredient(
-    val quantity: String? = "",
-)
 
 class RecipeViewModel : ViewModel(){
 }
