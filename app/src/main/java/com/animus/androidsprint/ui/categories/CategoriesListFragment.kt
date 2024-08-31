2e2f30d92b499a656ee703e116b0d05d0529e4a4
= <=>
2024-08-31 16:22:07 +0500

    RA-27 mvvm all fragments 1. Added Log to CategoriesListFragment.kt.kt

--- app/src/main/java/com/animus/androidsprint/ui/categories/CategoriesListFragment.kt
+++ app/src/main/java/com/animus/androidsprint/ui/categories/CategoriesListFragment.kt
@@ -1,6 +1,7 @@
 package com.animus.androidsprint.ui.categories
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -29,6 +30,7 @@ class CategoriesListFragment : Fragment() {
     ): View {
         _binding = FragmentListCategoriesBinding.inflate(inflater, container, false)
         val view = binding.root
+        Log.e("!!!", "CategoriesListFragment create")
         return view
     }
 
@@ -40,6 +42,7 @@ class CategoriesListFragment : Fragment() {
     }
 
     override fun onDestroyView() {
+        Log.e("!!!", "CategoriesListFragment destroy")
         super.onDestroyView()
         _binding = null
     }
