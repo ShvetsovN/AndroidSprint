aff36784528823b34f2edd50bc0e057de0121026
= <=>
2024-09-20 12:37:23 +0500

    RA-29 jetpack navigation safe args 1. Updated safe args plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,6 @@ dependencies {
     implementation(libs.androidx.navigation.fragment)
     implementation(libs.androidx.navigation.ui)
     implementation(libs.androidx.navigation.dynamic.features.fragment)
-    implementation(libs.androidx.navigation.navigation.safe.args)
 
     implementation(libs.androidx.fragment)
     implementation(libs.androidx.core.ktx)
