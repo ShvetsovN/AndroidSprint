78459af0828427479f6a548b96b29104df82e5be
= <=>
2024-09-26 16:04:54 +0500

    RA-32 network URLconnection 1. added new thread in MainActivity.kt

--- app/src/main/java/com/animus/androidsprint/MainActivity.kt
+++ app/src/main/java/com/animus/androidsprint/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.animus.androidsprint
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
-import androidx.navigation.navOptions
 import com.animus.androidsprint.databinding.ActivityMainBinding
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,12 +30,32 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val thread = Thread {
+            val url = URL("https://recipes.androidsprint.ru/api/category")
+            val connection = url.openConnection() as HttpURLConnection
+            connection.connect()
+
+            Log.i("!!!", "Выполняю запрос на потоке: ${Thread.currentThread().name}")
+            Log.i("!!!", "Body: ${connection.inputStream.bufferedReader().readText()}")
+        }
+        thread.start()
+
+        Log.i("!!!", "Метод onCreate() выполняется на потоке: ${Thread.currentThread().name}")
+
         with(binding) {
             btnFavorites.setOnClickListener {
-                findNavController(R.id.nav_host_fragment).navigate(R.id.favoritesFragment, null, navOption)
+                findNavController(R.id.nav_host_fragment).navigate(
+                    R.id.favoritesFragment,
+                    null,
+                    navOption
+                )
             }
             btnCategory.setOnClickListener {
-                findNavController(R.id.nav_host_fragment).navigate(R.id.categoriesListFragment, null, navOption)
+                findNavController(R.id.nav_host_fragment).navigate(
+                    R.id.categoriesListFragment,
+                    null,
+                    navOption
+                )
             }
         }
     }
