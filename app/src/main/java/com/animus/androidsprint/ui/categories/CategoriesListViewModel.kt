59adcfdedf57b3099ab76ba911b986a2f7b32656
= <=>
2024-10-01 19:25:45 +0500

    RA-35 network Retrofit 1. Trying to redesign first viewModel (CategoriesListViewModel)

--- app/src/main/java/com/animus/androidsprint/ui/categories/CategoriesListViewModel.kt
+++ app/src/main/java/com/animus/androidsprint/ui/categories/CategoriesListViewModel.kt
@@ -1,16 +1,21 @@
 package com.animus.androidsprint.ui.categories
 
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.animus.androidsprint.data.STUB
+import com.animus.androidsprint.data.RecipeRepository
 import com.animus.androidsprint.model.Category
+import java.util.concurrent.Executors
 
 class CategoriesListViewModel : ViewModel() {
 
     private val _categoriesListLiveData = MutableLiveData<CategoriesListState>()
     val categoriesListLiveData: LiveData<CategoriesListState> = _categoriesListLiveData
+
+    private val repository = RecipeRepository()
+    private val threadPool = Executors.newFixedThreadPool(2)
 
     init {
         Log.e("CategoriesListVM", "VM created")
@@ -18,10 +23,14 @@ class CategoriesListViewModel : ViewModel() {
     }
 
     private fun loadCategories() {
-        val categories: List<Category> = STUB.getCategories()
-        _categoriesListLiveData.value = CategoriesListState(
-            categories = categories
-        )
+        threadPool.execute {
+            val categories = repository.getCategories()
+            if (categories == null) {
+                Toast.makeText(context, "Ошибка получения данных", Toast.LENGTH_SHORT).show()
+            } else {
+                _categoriesListLiveData.value = CategoriesListState(categories = categories)
+            }
+        }
     }
 
     data class CategoriesListState(
