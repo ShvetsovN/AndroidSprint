2ffe578a81c60c0af24d17204b833c845a6f5379
= <=>
2024-10-24 12:27:13 +0500

    RA-41 Jetpack Room Favorites 1. Added isFavorite val to Recipe.kt

--- app/src/main/java/com/animus/androidsprint/model/Recipe.kt
+++ app/src/main/java/com/animus/androidsprint/model/Recipe.kt
@@ -13,5 +13,5 @@ data class Recipe(
     @ColumnInfo("method") val method: List<String>,
     @ColumnInfo("imageUrl") val imageUrl: String,
     @ColumnInfo("categoryId") val categoryId: Int = -1,
-    @ColumnInfo("isFavorite") val isFavorite: Boolean,
+    @ColumnInfo("isFavorite") val isFavorite: Boolean = false,
 ) : java.io.Serializable
