04e98e0b1bee63217613baffce1cfd41b018f4b2
= <=>
2024-11-05 14:44:03 +0500

    RA-43 DI hilt 1. Added annotation @Singleton to all Provides method 2. Removed all custom fabric

--- app/src/main/java/com/animus/androidsprint/di/RecipeModule.kt
+++ app/src/main/java/com/animus/androidsprint/di/RecipeModule.kt
@@ -18,12 +18,14 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class RecipeModule {
 
     @Provides
+    @Singleton
     fun provideDatabase(@ApplicationContext context: Context): AppDatabase =
         Room.databaseBuilder(
             context,
@@ -32,12 +34,15 @@ class RecipeModule {
         ).build()
 
     @Provides
+    @Singleton
     fun provideCategoriesDao(appDatabase: AppDatabase) : CategoriesDao = appDatabase.categoryDao()
 
     @Provides
+    @Singleton
     fun provideRecipesDao(appDatabase: AppDatabase) : RecipesDao = appDatabase.recipesDao()
 
     @Provides
+    @Singleton
     fun provideHttpClient(): OkHttpClient {
         val logging = HttpLoggingInterceptor()
         logging.setLevel(HttpLoggingInterceptor.Level.BODY)
@@ -47,16 +52,19 @@ class RecipeModule {
     }
 
     @Provides
+    @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         val contentType = Constants.CONTENT_TYPE.toMediaType()
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
             .addConverterFactory(Json.asConverterFactory(contentType))
+            .client(okHttpClient)
             .build()
         return retrofit
     }
 
     @Provides
+    @Singleton
     fun provideRecipeApiService(retrofit: Retrofit): RecipeApiService {
         return retrofit.create(RecipeApiService::class.java)
     }
